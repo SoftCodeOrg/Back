efefef4ef5efb88c8eeb8e082b2de9029a9e144c
kirill <vladimirov_kv@sensey.dev>
2023-11-24 16:29:01 +0500

    f

---
+++ Hakaton_UUNIT/WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace Hakaton_UUNIT;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int) (TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
